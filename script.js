66962b5e430659206177556723b487607df6a1bd
valbritos <valbritosv@gmail.com>
2023-04-20 02:06:39 -0300

    lol
    agregue imagenes nuevas, puse bordes en las cojitas de iniciar sesion y unirse, yyy eso creo, tmb canbie el tamaño de algunas letras y blabla

--- script.js
+++ script.js
@@ -1,87 +1,77 @@
-
-
 document.getElementById("btnInicioSesion").addEventListener("click", function() {
-    window.location.href = "InicioSesion.html";
-  });
-
+  window.location.href = "InicioSesion.html";
+});
 
 /*  CREAR CUENTA  */
 if (window.location.pathname.includes("Unete.html")) {
 
 
 const constraints = {
-    nombre: {
-      presence: true
-    },
-    correo: {
-      presence: true,
-      email: true
-    },
-    password: {
-      presence: true,
-    },
+  nombre: {
+    presence: true
+  },
+  correo: {
+    presence: true,
+    email: true
+  },
+  password: {
+    presence: true,
+  },
+};
+
+const formulario = document.querySelector('#formulario');
+formulario.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const datos = {
+    nombre: document.querySelector('#nombre').value,
+    correo: document.querySelector('#correo').value,
+    password: document.querySelector('#password').value
   };
-
-  const formulario = document.querySelector('#formulario');
-  formulario.addEventListener('submit', (e) => {
-    e.preventDefault();
-  
-    const datos = {
-      nombre: document.querySelector('#nombre').value,
-      correo: document.querySelector('#correo').value,
-      password: document.querySelector('#password').value
-    };
-  
-    fetch('https://jsonplaceholder.typicode.com/posts', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(datos)
-    })
-    .then(response => response.json())
-    .then(data => {
-      console.log(data);
-    })
-    .catch(error => {
-      console.error(error);
-    });
+  fetch('https://jsonplaceholder.typicode.com/posts', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(datos)
+  })
+  .then(response => response.json())
+  .then(data => {
+    console.log(data);
+  })
+  .catch(error => {
+    console.error(error);
   });
-  
-  
-
-
-
+});
 
 /*  INICIO DE SESION  */
 } else if (window.location.pathname.includes("InicioSesion.html")) /*verifica que estes en el html correcto*/{
-$(function() {
-  // Validación del formulario con Validate.js
-  const constraints = {
-    email: {
-      presence: true,
-      email: true
-    },
-    password: {
-      presence: true,
-      length: {
-        minimum: 6,
-        message: "La contraseña debe tener al menos 6 caracteres"
+  $(function() {
+    // Validación del formulario con Validate.js
+    const constraints = {
+      email: {
+        presence: true,
+        email: true
+      },
+      password: {
+        presence: true,
+        length: {
+          minimum: 6,
+          message: "La contraseña debe tener al menos 6 caracteres"
+        }
       }
-    }
-  };
+    };
 
-  const form = document.getElementById('loginForm');
-  form.addEventListener('submit', function(event) {
-    event.preventDefault();
-    const errors = validate(form, constraints);
-    if (errors) {
-      console.log(errors);
-    } else {
-      console.log('Formulario válido');
-      const email = form.email.value;
-      const password = form.password.value;
-      // Hacer una solicitud GET a la API
+    const form = document.getElementById('loginForm');
+    form.addEventListener('submit', function(event) {
+      event.preventDefault();
+      const errors = validate(form, constraints);
+      if (errors) {
+        console.log(errors);
+      } else {
+        console.log('Formulario válido');
+        const email = form.email.value;
+        const password = form.password.value;
+        // Hacer una solicitud GET a la API
       fetch(`               `)
         .then(response => response.json())
         .then(data => {
@@ -97,12 +87,7 @@ $(function() {
           console.log('Error al iniciar sesión');
           console.log(error);
         });
-    }
+      }
+    });
   });
-  
-
-});
-
-
-
- }
+}
